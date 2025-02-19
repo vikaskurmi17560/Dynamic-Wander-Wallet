d2e7281d17eca4fadf34ea617b23e6941155b2a8
lalitbisht2001 <lalitbisht2025@gmail.com>
2025-02-19 19:37:57 +0530

    show

--- frontend/src/components/NEWSPAPER/Newspaper.tsx
+++ frontend/src/components/NEWSPAPER/Newspaper.tsx
@@ -12,7 +12,7 @@ const Newspaper = () => {
       logo: "/images/Newspaper/yourstory.png",
       date: "Dec 02, 2024",
       comment:
-        "PickYourTrail is also carving a niche for itself in this space and intends to become a one-stop shop for personalised vacation",
+        "Wander-wallet is also carving a niche for itself in this space and intends to become a one-stop shop for personalised vacation",
     },
     {
       companyname: "The Hindu",
@@ -20,7 +20,7 @@ const Newspaper = () => {
       logo: "/images/Newspaper/hindu.avif",
       date: "Nov 21, 2024",
       comment:
-        "With offbeat experiences and a platter-full of trending destinations, PickYourTrail is helping change the way people travel",
+        "With offbeat experiences and a platter-full of trending destinations, Wander-wallet is helping change the way people travel",
     },
     {
       companyname: "nasscom",
@@ -28,7 +28,7 @@ const Newspaper = () => {
       logo: "/images/Newspaper/nasscom.avif",
       date: "Sep 23, 2024",
       comment:
-        "Tailor made to suit the needs of the travellers, Pickyourtrail offers a much-needed respite from packaged tours",
+        "Tailor made to suit the needs of the travellers, Wander-wallet offers a much-needed respite from packaged tours",
     },
     {
       companyname: "the economic times",
@@ -36,7 +36,7 @@ const Newspaper = () => {
       logo: "/images/Newspaper/economic.avif",
       date: "Feb 10, 2025",
       comment:
-        "Pickyourtrail uses its unique matching algorithms and price comparison engine to help travellers create customised tour packages at competitive online prices.",
+        "Wander-wallet uses its unique matching algorithms and price comparison engine to help travellers create customised tour packages at competitive online prices.",
     },
     {
       companyname: "times of india",
@@ -44,7 +44,7 @@ const Newspaper = () => {
       logo: "/images/Newspaper/toi.jpg",
       date: "Jan 04, 2024",
       comment:
-        "Pickyourtrail has helped book international vacations for over 10,000 travelers since 2014",
+        "Wander-wallet has helped book international vacations for over 10,000 travelers since 2014",
     },
   ];
 
