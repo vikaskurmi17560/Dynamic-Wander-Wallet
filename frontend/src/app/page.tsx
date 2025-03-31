d7f4a7d5dbabace1b7e75f76550ce140bd240ea1
vikaskurmi17560 <vkurmi307@gmail.com>
2025-03-31 21:48:34 +0530

    remove some extra pages and its related ui and tailwind.config.ts code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,11 @@
-import BudgetCardSection from "@/components/BudgetCardSection";
 import Destination from "@/components/DEST-GUIDE/Destination";
 import DestinationGuide from "@/components/DEST-GUIDE/DestinationGuide";
 import ExplorePage from "@/components/ExplorePage";
 import Feedback from "@/components/FEEDBACK/Feedback";
-import Footer from "@/components/FOOTER/Footer";
-import Footersection from "@/components/Footersection";
 import HeroSection from "@/components/Herosection"
 import HiddenPlaces from "@/components/HIDDEN_PLACE/HiddenPlaces";
 import HolidayBudget from "@/components/HolidayBudget";
 import Newspaper from "@/components/NEWSPAPER/Newspaper";
-import Reviewsection from "@/components/Reviewsection";
 import TripPurpose from "@/components/TripPurpose";
 import UniquePlaces from "@/components/UniquePlaces";
 import Waterfalls from "@/components/WATERFALLS/Waterfalls";
@@ -17,7 +13,6 @@ export default function Home() {
   return (
     <div className="flex flex-col bg-white" >
       <HeroSection />
-      {/* <BudgetCardSection /> */}
       <Waterfalls />
       <HiddenPlaces />
       <ExplorePage />
@@ -28,9 +23,6 @@ export default function Home() {
       <Destination />
       <DestinationGuide />
       <Feedback />
-      {/* <Reviewsection /> */}
-      <Footersection />
-      {/* <Footer /> */}
     </div>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -17,29 +17,6 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        scrollX: "scrollX 40s linear infinite",
-        scrollY: "scrollY 20s linear infinite",
-        aurora: "aurora 60s linear infinite",
-      },
-      keyframes: {
-        scrollX: {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-50%)" },
-        },
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
-          },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
-          },
-        },
-        scrollY: {
-          from: { transform: "translateY(0)" },
-          to: { transform: "translateY(-100%)" }, // Moves items upwards
-        },
-      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
@@ -47,42 +24,5 @@ export default {
     },
   },
  
-    plugins: [require('tailwind-scrollbar-hide'),
-      function ({ matchUtilities, theme }: any) {
-        matchUtilities(
-          {
-            "bg-grid": (value: any) => ({
-              backgroundImage: `url("${svgToDataUri(
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-              )}")`,
-            }),
-            "bg-grid-small": (value: any) => ({
-              backgroundImage: `url("${svgToDataUri(
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-              )}")`,
-            }),
-            "bg-dot": (value: any) => ({
-              backgroundImage: `url("${svgToDataUri(
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-              )}")`,
-            }),
-          },
-          { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-        );
-      },
-
-      addVariablesForColors
-    ],
  
 } satisfies Config;
-
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
- 
-  addBase({
-    ":root": newVars,
-  });
-}
